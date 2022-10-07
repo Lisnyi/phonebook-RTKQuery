6089edaf8ba02550652debb789f4e6449c55f89a
Lisnyi <russelhobbbs@gmail.com>
2022-10-07 17:09:36 +0300

    refactor to hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,47 +1,38 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { nanoid } from 'nanoid'
 import { ContactForm, ContactList, Filter } from '../../components'
 import { Box, MainTitle, SectionTitle } from './App.styled';
 
-export default class App extends Component {
+export const App = () => {
+  const [contacts, setContacts] = useState([])
+  const [filter, setFilter] = useState('')
 
-  state = {
-    contacts: [],
-    filter: ''
-  }
+  const filteredContacts = getFilteredContacts()
 
-  componentDidMount () {
+  useEffect(()=>{
     const newContacts = JSON.parse(localStorage.getItem("contacts"))
     if (newContacts?.length) {
-      this.setState({
-        contacts: newContacts
-      })
+      setContacts(newContacts)
     }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("contacts", JSON.stringify(contacts))
+  },[contacts])
+
+  function handleChange ({currentTarget}) {
+    setFilter(currentTarget.value)
   }
 
-  componentDidUpdate (_, prevState) {
-    const { contacts } = this.state
-    if ( prevState.contacts !== contacts) {
-      localStorage.setItem("contacts", JSON.stringify(contacts))
-    }
-  }
-  
-  handleChange = ({currentTarget}) => {
-    this.setState({
-      [currentTarget.name]:currentTarget.value
-    })
-  }
-
-  isDuplicate(name) {
-    const { contacts } = this.state
+  function isDuplicate (name) {
     const normalizedName = name.toLocaleLowerCase()
     const result = contacts.find((contact) => contact.name.toLocaleLowerCase() === normalizedName)
     return result
   }
-  
-  addContact = (name, number) => {
-    if (this.isDuplicate(name)) {
+
+  function addContact (name, number) {
+    if (isDuplicate(name)) {
       return Notify.warning(`${name} is already in contacts`)
     }
     const newContact = {
@@ -49,26 +40,14 @@ export default class App extends Component {
       name,
       number
     }
-    this.setState((prev)=>{
-        return {
-          contacts: [...prev.contacts, newContact]
-        }
-      })
+    setContacts((prev)=>([...prev, newContact]))
   }
 
-  deleteContact = (id) => {
-    this.setState((prev) => {
-        const contactsList = prev.contacts.filter((contact) => contact.id !== id);
-
-        return {
-            contacts: contactsList
-        }
-    })
+  function deleteContact (id) {
+    setContacts((prev) => prev.filter((contact) => contact.id !== id))
   }
 
-  getFilteredContacts = () => {
-    const { contacts, filter } = this.state
-    
+  function getFilteredContacts () {
     if (!filter) {
         return contacts
     }
@@ -83,27 +62,20 @@ export default class App extends Component {
     return filteredContacts;
   }
 
-  render() {
-    const { handleChange, addContact, getFilteredContacts, deleteContact } = this
-    const { filter } = this.state
-    const contacts = getFilteredContacts()
-
-    return (
-      <>
-        <Box>
-          <MainTitle>Phonebook</MainTitle>
-          <ContactForm addContact={addContact}/>
-        </Box>
-    
-        <Box>
-            <SectionTitle>Contacts</SectionTitle>
-            <Filter filter={filter} handleChange={handleChange}/>
-            {contacts.length
-              ? <ContactList contacts={contacts} deleteContact={deleteContact}/>
-              : null}
-        </Box>
-      </>
-    );
-  }
+  return (
+    <>
+      <Box>
+        <MainTitle>Phonebook</MainTitle>
+        <ContactForm addContact={addContact}/>
+      </Box>
+  
+      <Box>
+          <SectionTitle>Contacts</SectionTitle>
+          <Filter filter={filter} handleChange={handleChange}/>
+          {contacts.length
+            ? <ContactList contacts={filteredContacts} deleteContact={deleteContact}/>
+            : null}
+      </Box>
+    </>
+  )
 }
-
